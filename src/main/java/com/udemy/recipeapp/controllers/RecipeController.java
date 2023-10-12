7a5f6f0a5086fb100c405f226a6f50a07ffc79be
Guillaume <breyerguillaume@gmail.com>
2023-10-12 14:17:10 +0200

    Init CRUD - PostRecipe

--- src/main/java/com/udemy/recipeapp/controllers/RecipeController.java
+++ src/main/java/com/udemy/recipeapp/controllers/RecipeController.java
@@ -1,11 +1,11 @@
 package com.udemy.recipeapp.controllers;
 
+import com.udemy.recipeapp.commands.RecipeCommand;
 import com.udemy.recipeapp.services.RecipeServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @Slf4j
@@ -21,5 +21,19 @@ public class RecipeController {
         log.info("I'm in controller Recipe");
         model.addAttribute("recipe",recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe",new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("recipe/")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+        return "redirect:/recipe/show/"+savedCommand.getId();
+
     }
 }
